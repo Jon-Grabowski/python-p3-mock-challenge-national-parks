c4c549436e06e4e1f15c3b07a3a5e9fba0957af8
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-10 09:51:01 -0400

    basic setup done

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,6 +4,18 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
+
+    def get_name(self):
+        return self._name
+    def set_name(self, name):
+        if not hasattr(self, "name"):
+            if isinstance(name, str):
+                self._name = name
+            else:
+                raise Exception("Please enter valid name")
+        else:
+            raise Exception("Name can't be changed.")
+    name = property(get_name, set_name)
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -1,12 +1,27 @@
 class Visitor:
+    name_lock = True
 
-    def __init__(self, name):
+    def __init__(self, name):      
         self.name = name
-        
+
+    def get_name(self):
+        return self._name
+    def set_name(self, name):
+        if self.name_lock:
+            if isinstance(name, str) and 1 <= len(name) <= 15:
+                self._name = name
+                self.name_lock = False
+            else:
+                raise Exception("Please enter valid name")
+        else:
+            raise Exception("Name can't be changed.")
+    name = property(get_name, set_name)  
+
+
     def trips(self, new_trip=None):
         from classes.trip import Trip
         pass
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        pass 
